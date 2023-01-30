adb8a8873a11ceba6e8aa28365a923f792a9baa5
Samueelx <gsammygitau@gmail.com>
2023-01-30 12:14:45 +0300

    Completed.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const element = actors.map((actor, index) => {
+    return <div key={index}>
+      <h3>Name: {actor.name}</h3>
+      <ul>
+        {actor.movies.map((movie, i) => <li key={i}>{movie}</li>)}
+      </ul>
+    </div>
+  });
+  return <div>
+    <h1>Actors Page</h1>
+    {element}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const element = movies.map((movie, index) => {
+    return (<div key={index}>
+      <h3>{movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      <p>Genres: </p>
+      <ul>
+        {movie.genres.map((genre, i) => <li key={i}>{genre}</li>)}
+      </ul>
+    </div>);
+  });
+
+
+  return <div>
+    <h1>Movies Page</h1>
+    {element}
+  </div>;
 }
 
 export default Movies;
